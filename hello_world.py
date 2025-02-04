fe238146ca2cd195a497ade2098617574581c24a
Mace245 <133204082+Mace245@users.noreply.github.com>
2025-02-04 16:33:34 +0700

    Removing deprecated files, saving panda ipynb for future communication

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Prints 'Hello, World!' to the console."""
-
-def main():
-    # Use print function to display the message
-    print("Hello, World!")
-
-if __name__ == "__main__":
-    main()
--- testingpd.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
- 
-# date string
-d_string = "2023-09-17 14:30:00"
- 
-# Convert the string to datetime
-dt_obj = pd.to_datetime(d_string)
- 
-print(dt_obj.hour)
