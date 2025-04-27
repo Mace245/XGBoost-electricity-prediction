a43a46d65bd272a286ae962798d45075667aab13
Mace245 <133204082+Mace245@users.noreply.github.com>
2025-04-27 15:47:06 +0700

    added mrp endoint

--- lib/train.py
+++ lib/train.py
@@ -2,19 +2,28 @@ import algo, data
 import pickle
 import pandas as pd
 
+LATITUDE = 14.5833
+LONGITUDE = 121.0
+
 # Define features
 features = [
     'hour', 'day_of_week', 'day_of_month', 'is_weekend',
-
     'lag_1','lag_24','lag_168',
     'temperature'
 ]
 target = 'Wh'
 
 def train(predict:bool, forecast_period_days:int):
-    training_data = data.create_training_data(features,target)
+    training_data = pd.read_csv('training_data.csv', parse_dates=['DateTime'])
+    training_data.set_index('DateTime', inplace=True)
+    temp_col = training_data.pop('temperature')
+    training_data.insert(8, 'temperature', temp_col)
+
+    target_data=training_data.drop(columns='Wh')
+    feature_data=training_data.loc[:, 'Wh']
+
     # Train model
-    model = algo.train_xgboost_model(training_data.X, training_data.y)
+    model = algo.train_xgboost_model(target_data, feature_data)
 
     # Save artifacts
     with open('model.pkl', 'wb') as f:
@@ -23,11 +32,18 @@ def train(predict:bool, forecast_period_days:int):
     if predict:
         forecast_period = pd.Timedelta(days=forecast_period_days)
 
+        data.temp_fetch('2023-03-08','2023-03-14', LATITUDE, LONGITUDE, historical=True).to_csv("test.csv")
         # Generate forecast
-        last_known_data = training_data.X.loc[training_data.X.index > training_data.X.index.max() - forecast_period]  # Last week of data
-        forecast = algo.predict_on_window(model, last_known_data)
-        print(model, last_known_data)
+        last_known_data = target_data.loc[target_data.index > target_data.index.max() - forecast_period]  # Last week of data
+        forecast = algo.predict_on_window_recursive(
+            model=model, 
+            history_df= training_data, 
+            future_temps_series=data.temp_fetch('2023-03-07','2023-03-14', LATITUDE, LONGITUDE, historical=True), hours_to_forecast=forecast_period_days*24, model_features=features, target_variable=target, max_lag=168)
+        print(last_known_data)
 
-        data.visualize(model, features, training_data.y, forecast, forecast_period)
+        forecast.to_csv('forecast.csv')
 
-train(True, 365*2)
+        data.visualize(model, features, feature_data, forecast, forecast_period)
+
+# data.create_training_data(features,target)
+train(True, 7)
